1f2af7840380c03467fbe19598a9d0f1cc397847
Asif Sayyad <asifin90@gmail.com>
2021-01-29 15:27:56 +0530

    Add files via upload

---
+++ TagHelpers/TagHelpers/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpers.Models
+{
+    public class Employee
+    {
+        public int EmployeId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
