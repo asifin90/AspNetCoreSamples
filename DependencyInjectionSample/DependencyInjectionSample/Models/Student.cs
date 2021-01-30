c3edbe98863cd5bef3f186c3007d00a240c5fae7
Asif Sayyad <asifin90@gmail.com>
2021-01-31 02:41:11 +0530

    Add files via upload

---
+++ DependencyInjectionSample/DependencyInjectionSample/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionSample.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        public string City { get; set; }
+
+    }
+}
