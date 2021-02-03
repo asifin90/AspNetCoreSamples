1ee615724a14ac45cb88b0ae5ef200c702d3c380
Asif Sayyad <asifin90@gmail.com>
2021-02-03 23:13:35 +0530

    Add files via upload

---
+++ E_MobileShop/Begin/MobileApplication/MobileApplication/Models/MobileDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileApplication.Models
+{
+    public class MobileDbContext:DbContext
+    {
+        public MobileDbContext(DbContextOptions<MobileDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MobileBrand> Brand { get; set; }
+
+    }
+}
